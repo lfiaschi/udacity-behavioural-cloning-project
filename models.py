74c8d919b799379221f79dadf8cf4284531682a4
Luca Fiaschi <luca.fiaschi@rocket-internet.com>
2017-04-17 10:13:10 +0200

    refactor: Added documentation removed Elu version of Nvidia model

--- models.py
+++ models.py
@@ -6,6 +6,12 @@ from keras.layers import Cropping2D
 
 
 def Simple(input_shape):
+    """
+    Super simple model just normaliztion and single layer
+    :param input_shape: 
+    :return: 
+    """
+
     model = Sequential()
 
     #normalization
@@ -49,13 +55,25 @@ def LeNetKerasMSE(input_shape, dropout=.3 ):
 
 
 def NvidiaNet(input_shape, dropout = .3):
+    """
+    Implement the Nvidia model for self driving cars
+    https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/
+    :param input_shape: shape of the images (r,w,c)
+    :param dropout: 
+    :return: 
+    """
+
     model = Sequential()
 
     # normalization
     model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=input_shape))
-    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Cropping2D(cropping=((65, 25), (0, 0))))
 
-    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2,2), activation='relu'))
+    # Allow the model to choose the appropriate color space
+    # https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9
+    model.add(Conv2D(3, kernel_size=(1, 1), strides=(1, 1), activation='linear'))
+
+    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
     model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
     model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
 
@@ -63,7 +81,6 @@ def NvidiaNet(input_shape, dropout = .3):
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 
     model.add(Flatten())
-
     model.add(Dense(1164, activation='relu'))
     model.add(Dropout(dropout))
     model.add(Dense(100, activation='relu'))
@@ -71,6 +88,7 @@ def NvidiaNet(input_shape, dropout = .3):
     model.add(Dense(50, activation='relu'))
     model.add(Dropout(dropout))
     model.add(Dense(10, activation='relu'))
+
     model.add(Dense(1, activation='linear'))
 
     return model
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-from models import LeNetKerasMSE, Simple
-from data_pipe import load_meta, generator
+from models import LeNetKerasMSE, Simple, NvidiaNet
+from data_pipe import *
 from keras.callbacks import EarlyStopping
 from math import ceil
 from matplotlib import pyplot as plt
@@ -9,21 +9,30 @@ plt.switch_backend('agg')
 outmodelname = 'models/model.h5'
 
 ch, row, col = 3, 160, 320  # Image shape
-batch_size = 128
+batch_size = 256
+keep_prob = .9
+min_angle = -1000.0
+max_angle = 1000.0
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 
-train_samples, validation_samples = load_meta()
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(train_samples, batch_size=batch_size)
+train_samples, validation_samples = load_meta_new(exclude_track=None)
 
-model = Simple((row,col,ch))
+train_generator = generator_new(train_samples,
+                                batch_size,
+                                keep_prob=keep_prob,
+                                min_angle = min_angle,
+                                max_angle = max_angle)
+
+validation_generator = generator_validation(validation_samples,
+                                            batch_size=batch_size)
+
+model = NvidiaNet((row,col,ch), dropout=.3)
 
 model.compile(loss='mse', optimizer='adam')
 
-
-steps_per_epoch = ceil(len(train_samples) / batch_size)
-validation_steps = ceil(len(validation_samples) / batch_size)
+steps_per_epoch = len(train_samples) // batch_size
+validation_steps = len(validation_samples) // batch_size
 
 print('Number of training steps {}'.format(steps_per_epoch))
 print('Number of validation steps {}'.format(validation_steps))
@@ -32,7 +41,7 @@ history_object = model.fit_generator(train_generator,
                                      steps_per_epoch=steps_per_epoch,
                                      validation_data=validation_generator,
                                      validation_steps=validation_steps,
-                                     epochs=5,
+                                     epochs=100,
                                      callbacks=[early_stopping],
                                      verbose = 1
                                      )
